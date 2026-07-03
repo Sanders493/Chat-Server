f56ac494bab24de0f6f72ae9b7c8b706f7be4916
sanders93 <stshinyama@blc.edu>
2026-07-02 19:41:09 -0500

    Finished the greet_user, user_farewell and broadcast methods

--- src/chat_server.py
+++ src/chat_server.py
@@ -2,13 +2,30 @@ import asyncio
 import re
 from client import Client
 
-HOST = "192.0.0.3"
+HOST = "192.0.0.2"
 PORT = 8000
 
 clients: list[Client] = []
 
 lock = asyncio.Lock()
 
+async def broadcast(message, username):
+    """ Sends message to all clients except sender """
+    
+    dead_clients = []
+    
+    for client in clients.copy():
+        if client.username != username:
+            try:
+                client.writer.write(message)
+                await client.writer.drain()
+            except ConnectionError:
+                dead_clients.append(client)
+    
+    async with lock:
+        for client in dead_clients:
+            clients.remove(client)
+        
 async def get_username(reader, writer):
     """Get the username of a client"""
     
@@ -39,7 +56,26 @@ async def greet_user(writer, username):
     """Greet the user with the appropriate message based on their username""" 
     
     writer.write(f"Welcome to the chat {username}!\n".encode())
+    await broadcast(f"{username} joined\n".encode(), username)
+
+async def user_farewell(writer, username, addr):
+    """Execute the user farewell operations"""
     
+    print(f"Disconnected: {username} @ {addr}")
+    await broadcast(f"{username} left\n".encode(), username)
+        
+    await remove_user_from_client_ls(username)
+    writer.close()
+    await writer.wait_closed()
+    
+
+async def remove_user_from_client_ls(username):
+    """ Remove a user from the client list"""  
+    for client in clients:
+        if client.username == username:
+            clients.remove(client)
+
+  
 async def handle_client(reader, writer):
     """ Handle a new client 
 
@@ -64,15 +100,16 @@ async def handle_client(reader, writer):
                 break
             
             message = data.decode().strip()
-            print(message)
+            print(f"{username}: {message}")
+            
+            formatted = f"{username}: {message}\n".encode()
+            await broadcast(formatted, username)
             
     except Exception as e:
         print(e)
         
     finally:
-        print(f"Disconnected: {addr}")
-        writer.close()
-        await writer.wait_closed()
+        await user_farewell(writer, username, addr)
         
 
 async def run_server():
